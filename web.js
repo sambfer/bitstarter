f3ab46edcc567aaa571ade2eefad1fea3f862f3c
sambfer <ferreira.samuel@gmail.com>
2013-07-13 15:22:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    fs.readFile('index.html', function (err, data) {
+    fs.readFile('index.html',r, function (err, data) {
       if (err) throw err;
-      response.send(data);
     });	
+    response.send(data);	
 });	
 
-//});
+//}); Test to send data 
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
